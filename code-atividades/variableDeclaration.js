89b22ec4c7a07568cb7eee27b0b60c8239ae4ceb
Ana Luiza <analuizanc13@gmail.com>
2024-10-22 19:03:39 +0000

    rename python file and implementation of variables declaration and showing their types done

---
+++ code-atividades/variableDeclaration.js
@@ -0,0 +1,11 @@
+
+let name = "Michael";
+let number = 2;
+let numberFloat = 5.7;
+
+console.log("Name:", name, "\nNumber:", number, "\nNumber float:", numberFloat);
+
+console.log(name, "is of type", typeof(name));
+console.log(number,"is of type",typeof(number));
+console.log(numberFloat,"is of type",typeof(numberFloat));
+
